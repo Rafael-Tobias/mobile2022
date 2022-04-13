7e55021ed7280eeb0ca4e0035dcee21e149b6403
Rafael-Tobias <rafaaugusto.tobias@gmail.com>
2022-04-13 08:55:40 -0300

    Aula 13/04 atualizar

--- App.tsx
+++ App.tsx
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 import Navigation from "./src/navigations";
 
 export default function App() {
   return <Navigation />;
 }
-=======
-import Login from "./src/screens/Login"
-
-export default function App() {
-  return <Login />
-}
->>>>>>> df6bd920540815a35e0d099300471c1accc27e91
